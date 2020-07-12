f3ca7504a356a6658ce2290a75746919af374723
xXNurioXx <xxnurioxx@gmail.com>
2020-07-12 06:02:08 +0200

    Acquired ability to drive to a point

--- src/gta-samp-driver/Bot.cpp
+++ src/gta-samp-driver/Bot.cpp
@@ -1,32 +1,64 @@
 #include <iostream>
 #include <Windows.h>
 #include "Movement.cpp"
-#include "GameResources.cpp"
+#include "AxisManager.cpp"
 
-Movement* BOT = new Movement();
-GameResources* GTA = new GameResources();
+Movement BOT = Movement();
+GameResources gameResources = GameResources();
+AxisCalculation axisCalculation = AxisCalculation(gameResources);
+MathTrigonometry *mathTrigonometry = new MathTrigonometry(gameResources);
 
-int main()
-{
-	int destX = -360;
-	int destY = 2150;
+float destX = -2001;
+float destY = -285;
 
-	while (true) {
+void move() {
+    float distance = axisCalculation.GetDistanceTo(destX, destY);
+    MovingDirection movingTo = axisCalculation.GetDirectionTo(destX, destY);
+    string direction = movingTo == FORWARD ? "forward" : (movingTo == LEFT) ? "left" : "right";
+    printf("Moving to %s\n", direction.c_str());
 
-		Sleep(800);
-		if (!GTA->IsInCar()) {
-			printf("Player is not in a car.\n");
-			continue;
-		}
+    if (distance >= 10) {
+        BOT.MoveForward();
+    } else {
+        BOT.StopMovingForward();
+    }
 
-		float posX = GTA->GetPlayerX();
-		float posY = GTA->GetPlayerY();
-		float posZ = GTA->GetPlayerZ();
-		printf("POS ( %f , %f )\n", posX, posY);
+    if (movingTo == FORWARD) {
+        BOT.StopMovingLeft();
+        BOT.StopMovingRight();
+    } else if (movingTo == LEFT) {
+        BOT.StopMovingRight();
+        BOT.MoveLeft();
+    } else {
+        BOT.StopMovingLeft();
+        BOT.MoveRight();
+    }
+}
 
-		float rotation = GTA->GetCarRotationAngle();
-		printf("ROT ( %f )\n", rotation);
 
-	}
+void drive() {
+    Sleep(10);
+    if (!gameResources.IsInCar()) {
+        printf("Player is not in a car.\n");
+        Sleep(500);
+        return;
+    }
 
+    float distance = axisCalculation.GetDistanceTo(destX, destY);
+    if (distance <= 1) {
+        printf("Destination point too close.\n");
+        BOT.StopMovingForward();
+        BOT.StopMovingLeft();
+        BOT.StopMovingRight();
+        return;
+    }
+    if (distance >= 100) printf("Destination point too far.\n");
+
+    move();
+}
+
+int main() {
+    while (true) {
+        drive();
+    }
 }
