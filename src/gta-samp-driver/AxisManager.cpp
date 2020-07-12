77f37cca4328f0e9efd2537570af229f3dfac72b
xXNurioXx <xxnurioxx@gmail.com>
2020-07-12 04:53:57 +0200

    Added trigonometry calculations

---
+++ src/gta-samp-driver/AxisManager.cpp
@@ -0,0 +1,54 @@
+#include "MathTrigonometry.cpp"
+
+enum FacingDirection {
+    NORTH, EAST, SOUTH, WEST
+};
+
+enum MovingDirection {
+    LEFT, FORWARD, RIGHT
+};
+
+class AxisCalculation {
+
+private:
+    GameResources gameResources;
+    MathTrigonometry *mathTrigonometry;
+
+public:
+    AxisCalculation(GameResources gameResources) {
+        this->gameResources = gameResources;
+        this->mathTrigonometry = new MathTrigonometry(gameResources);
+    }
+
+public:
+    FacingDirection GetFacingDirection() {
+        float angle = gameResources.GetCarRotationAngle();
+        if (angle >= 45 && angle <= 134) return EAST;
+        if (angle >= 135 && angle <= 224) return SOUTH;
+        if (angle >= 225 && angle <= 315) return WEST;
+        return NORTH;
+    }
+
+public:
+    MovingDirection GetDirectionTo(float destinationX, float destinationY) {
+        float desiredAngle = AngleFromCoordinate(destinationX, destinationY);
+        float carAngle = gameResources.GetCarRotationAngle();
+        double closeDistance = mathTrigonometry->calculateAngleDistance(desiredAngle, carAngle);
+
+        if (closeDistance <= 7 && closeDistance >= -7) return FORWARD;
+        return (closeDistance < 0) ? RIGHT : LEFT;
+    }
+
+public:
+    float GetDistanceTo(float destinationX, float destinationY) {
+        float carX = gameResources.GetPlayerX();
+        float carY = gameResources.GetPlayerY();
+        return mathTrigonometry->Distance(carX, carY, destinationX, destinationY);
+    }
+
+public:
+    float AngleFromCoordinate(float destX, float destY) {
+        return mathTrigonometry->GetAngleTo(destX, destY);
+    }
+
+};
---
+++ src/gta-samp-driver/MathTrigonometry.cpp
@@ -0,0 +1,104 @@
+#include "GameResources.cpp"
+
+enum PositionQuadrant {
+    CENTER,         // No quadrant, on an axis line.
+    PLUS_PLUS,      // Top right
+    PLUS_MINUS,     // Down right
+    MINUS_MINUS,    // Down left
+    MINUS_PLUS      // Top left
+};
+
+class MathTrigonometry {
+
+private:
+    GameResources gameResources;
+
+public:
+    MathTrigonometry(GameResources gameResources) {
+        this->gameResources = gameResources;
+    }
+
+public:
+    float Square(float number) {
+        return number * number;
+    }
+
+public:
+    float Distance(float originX, float destinationX) {
+        return sqrt(Square(originX - destinationX));
+    }
+
+public:
+    float Distance(float originX, float originY, float destinationX, float destinationY) {
+        return sqrt(Square(originX - destinationX) + Square(originY - destinationY));
+    }
+
+public:
+    PositionQuadrant GetRelativeQuadrantOf(float x, float y) {
+        float originX = gameResources.GetPlayerX();
+        float originY = gameResources.GetPlayerY();
+        if (originX == x && originY == y) return PLUS_PLUS;
+
+        if (x >= originX && y > originY) return PLUS_PLUS;
+        if (x > originX && y <= originY) return PLUS_MINUS;
+        if (x < originX && y <= originY) return MINUS_MINUS;
+        if (x <= originX && y != originY) return MINUS_PLUS;
+        throw -1;
+    }
+
+public:
+    float GetAttachedLineValue(float x, float y) {
+        float originX = gameResources.GetPlayerX();
+        float originY = gameResources.GetPlayerY();
+        PositionQuadrant quadrant = GetRelativeQuadrantOf(x, y);
+
+        if (quadrant == PLUS_PLUS || quadrant == MINUS_MINUS) return this->Distance(originY, y);
+        if (quadrant == PLUS_MINUS || quadrant == MINUS_PLUS) return this->Distance(originX, x);
+        throw -1;
+    }
+
+public:
+    float GetHiddenLineValue(float x, float y) {
+        float originX = gameResources.GetPlayerX();
+        float originY = gameResources.GetPlayerY();
+        PositionQuadrant quadrant = GetRelativeQuadrantOf(x, y);
+
+        if (quadrant == PLUS_PLUS || quadrant == MINUS_MINUS) return this->Distance(originX, x);
+        if (quadrant == PLUS_MINUS || quadrant == MINUS_PLUS) return this->Distance(originY, y);
+        throw -1;
+    }
+
+public:
+    float GetHypotenuseLineValue(float destinationX, float destinationY) {
+        float originX = gameResources.GetPlayerX();
+        float originY = gameResources.GetPlayerY();
+        return this->Distance(originX, originY, destinationX, destinationY);
+    }
+
+public:
+    int GetOffSetGrades(float x, float y) {
+        PositionQuadrant quadrant = GetRelativeQuadrantOf(x, y);
+        if (quadrant == CENTER) return 0;
+        if (quadrant == PLUS_PLUS) return 0;
+        if (quadrant == PLUS_MINUS) return 90;
+        if (quadrant == MINUS_MINUS) return 180;
+        if (quadrant == MINUS_PLUS) return 270;
+        throw -1;
+    }
+
+public:
+    double GetAngleTo(float x, float y) {
+        float attached = this->GetAttachedLineValue(x, y);
+        float hypotenuse = this->GetHypotenuseLineValue(x, y);
+
+        double angleAtt = acos(attached / hypotenuse) * 180.0 / M_PI;
+        return angleAtt + this->GetOffSetGrades(x, y);
+    }
+
+public:
+    double calculateAngleDistance(float angle1, float angle2) {
+        double diff = fmod((angle2 - angle1 + 180), 360) - 180;
+        return (diff < -180) ? diff + 360 : diff;
+    }
+
+};
