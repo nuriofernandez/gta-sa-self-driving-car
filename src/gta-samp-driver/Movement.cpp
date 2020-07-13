4969f05b5427152ce4eb4d4e5bf46b3d78679ec9
xXNurioXx <xxnurioxx@gmail.com>
2020-07-13 06:57:51 +0200

    Improved Movement manager, added brake operation.

--- src/gta-samp-driver/Movement.cpp
+++ src/gta-samp-driver/Movement.cpp
@@ -2,63 +2,86 @@
 
 class Movement {
 
-private: INPUT input;
+private:
+    INPUT input;
 
-public: Movement() {
-	input.type = INPUT_KEYBOARD;
-	input.ki.wScan = 0; // hardware scan code for key
-	input.ki.time = 0;
-	input.ki.dwExtraInfo = 0;
-}
+public:
+    Movement() {
+        input.type = INPUT_KEYBOARD;
+        input.ki.wScan = 0; // hardware scan code for key
+        input.ki.time = 0;
+        input.ki.dwExtraInfo = 0;
+    }
 
-public: void LongPressKey(int key) {
-	PressKey(key);
-	Sleep(200);
-	ReleaseKey(key);
-}
+private:
+    void longPressKey(int key) {
+        pressKey(key);
+        Sleep(200);
+        releaseKey(key);
+    }
 
-public: void PressKey(int key) {
-	input.ki.wVk = key; // virtual-key code for the key
-	input.ki.dwFlags = 0; // 0 for key press
-	SendInput(1, &input, sizeof(INPUT));
-}
+private:
+    void pressKey(int key) {
+        input.ki.wVk = key; // virtual-key code for the key
+        input.ki.dwFlags = 0; // 0 for key press
+        SendInput(1, &input, sizeof(INPUT));
+    }
 
-public:	void ReleaseKey(int key) {
-	input.ki.wVk = key; // virtual-key code.
-	input.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
-	SendInput(1, &input, sizeof(INPUT));
-}
+private:
+    void releaseKey(int key) {
+        input.ki.wVk = key; // virtual-key code.
+        input.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+        SendInput(1, &input, sizeof(INPUT));
+    }
 
-public: void MoveLeft() {
-	PressKey(0x41);
-}
+public:
+    void MoveLeft() {
+        pressKey(0x41);
+    }
 
-public: void StopMovingLeft() {
-	ReleaseKey(0x41);
-}
+public:
+    void StopMovingLeft() {
+        releaseKey(0x41);
+    }
 
-public: void MoveRight() {
-	PressKey(0x44);
-}
+public:
+    void MoveRight() {
+        pressKey(0x44);
+    }
 
-public:	void StopMovingRight() {
-	ReleaseKey(0x44);
-}
+public:
+    void StopMovingRight() {
+        releaseKey(0x44);
+    }
 
-public:	void MoveBack() {
-	PressKey(0x53);
-}
+public:
+    void MoveBack() {
+        pressKey(0x53);
+    }
 
-public:	void StopMovingBack() {
-	ReleaseKey(0x53);
-}
+public:
+    void StopMovingBack() {
+        releaseKey(0x53);
+    }
 
-public:	void MoveForward() {
-	PressKey(0x57);
-}
+public:
+    void MoveForward() {
+        pressKey(0x57);
+    }
 
-public:	void StopMovingForward() {
-	ReleaseKey(0x57);
-}
+public:
+    void StopMovingForward() {
+        releaseKey(0x57);
+    }
+
+public:
+    void Brake() {
+        pressKey(0x20);
+    }
+
+public:
+    void StopBrake() {
+        releaseKey(0x20);
+    }
 
 };
